38da91f3a365f29d72cf53f67bc1bdd3d7482663
Ygor Castor <ygor.castor@persgroep.net>
2022-03-28 17:53:40 -0300

    Fixing mix config

--- mix.exs
+++ mix.exs
@@ -8,6 +8,8 @@ defmodule Ravix.MixProject do
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      description: description(),
+      package: package(),
       elixirc_paths: elixirc_paths(Mix.env()),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
@@ -63,4 +65,15 @@ defmodule Ravix.MixProject do
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
+
+  defp description() do
+    "An Elixir driver for RavenDB"
+  end
+
+  defp package() do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/YgorCastor/ravix"}
+    ]
+  end
 end
